ba3ad8fc222985705a6c99b8a62236f6ba187e31
lgdcnm <18268815524@163.com>
2017-09-27 09:54:21 +0800

    js

--- js/width.js
+++ js/width.js
@@ -9,6 +9,14 @@
 			var width = oHtml.getBoundingClientRect().width;
 			
 			oHtml.style.fontSize = width/size + 'px';
+			var x = width/size;
+			
+			if(x>=56.8889){
+				x = 56.8889;
+			}
+			
+			
+			oHtml.style.fontSize =  x + 'px';
 			
 		}
 		
